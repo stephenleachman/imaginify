c1189265a91aea3ecb6b54394b2822665fe0168d
Stephen Leachman <globalumbrella16@gmail.com>
2024-11-15 21:55:53 -0500

    update

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,28 +1,22 @@
-import Header from '@/app/components/shared/header'
+import Header from '@/app/components/shared/header';
 import TransformationForm from '@/app/components/shared/TransformationForm';
-import { transformationTypes } from '@/constants'
+import { transformationTypes } from '@/constants';
 import { getUserById } from '@/lib/actions/user.actions';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
-
-// type SearchParamProps = {
-//   params: {
-//     type: keyof typeof transformationTypes; // Ensures 'type' matches a key in transformationTypes
-//   };
-// };
-
-// Now, your component definition:
 const AddTransformationTypePage = async ({ params }: SearchParamProps) => {
-  const resolvedParams = params; // Ensure params are awaited
+  // Ensure params are awaited properly
+  const resolvedParams = await params; // Await the params object before using
+
   const { type } = resolvedParams;
   const transformation = transformationTypes[type];
 
   const { userId } = await auth();
-
   if (!userId) redirect('sign-in');
 
   const user = await getUserById(userId);
+
   return (
     <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
